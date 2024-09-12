bfdc14839ee32b5c100bbc9fa23c16abaffce8db
Christian Ogrodny <cogrodny@luc.edu>
2024-09-12 15:17:20 -0500

    systimer added

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,10 +1,33 @@
 
 
+char glbl[128];
+
+char huge_array[8192];
 
 void kernel_main() {
 
-    // Another little comment
-    // Adding a comment
-    while(1){
+    extern int __bss_start, __bss_end;
+    char *bssstart, *bssend;
+
+    bssstart = &__bss_start;
+    bssend = &__bss_end;
+
+    unsigned long count = get_timer_count();    
+    unsigned long wait_count = wait_timer();
+    
+    while(bssstart < bssend){
+        *bssstart++ = 0;
     }
+
+}
+
+unsigned long get_timer_count() {
+    unsigned long *timer_count_register = 0x3f003004;
+    return *timer_count_register;
+}
+
+void wait_timer() {
+    unsigned long count = get_timer_count();
+    unsigned long wait_count = count + (milliseconds * 1000);
+    return wait_count;
 }
